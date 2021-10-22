d6f293dade517e9a3dbdbd457f0ce6c10de752f2
Marcos Albuquerque <marcos.albuquerque346@gmail.com>
2021-10-22 09:12:29 -0300

    improvement: novas implementações na classe de paginação

--- app/db/Pagination.php
+++ app/db/Pagination.php
@@ -19,6 +19,7 @@
       $this->limit = $limit;
       $this->results = $results;
       $this->currentPage = (is_numeric($currentPage) and $currentPage > 0) ? $currentPage : 1;
+      $this->calculate();
     }
 
     // método responsável por calcular a páginação
@@ -28,6 +29,30 @@
 
       // verifica se a página atual não excede o número de página
       $this->currentPage = $this->currentPage <= $this->pages ? $this->currentPage : $this->pages;
+    }
+
+    // método responsável por retornar a claúsula limit do SQL
+    public function getLimit(){
+      $offset = ($this->limit * ($this->currentPage - 1));
+      return $offset.','.$this->limit;
+    }
+
+    // método responsável por retornar as opções de páginas disponíveis
+    public function getPages(){
+      // não retornar páginas disponíveis
+      if($this->pages == 1) return [];
+
+      // páginas
+      $paginas = [];
+
+      for($i=1; $i<=$this->pages;$i++){
+        $paginas[] = [
+          'pagina' => $i,
+          'atual'   => $i == $this->currentPage
+        ];
+      }
+
+      return $paginas;
     }
   }
 ?>
