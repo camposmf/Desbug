8415ebce574cfb14f250787b7ff0ca6b77358f69
Jamile de Sousa <jamiledsousa@gmail.com>
2021-10-23 17:07:47 -0300

    feature: Implementando tela home

--- app/Controller/Pages/Home.php
+++ app/Controller/Pages/Home.php
@@ -1,27 +1,30 @@
 <?php
   namespace App\Controller\Pages;
   use \App\Utils\View;
-  use \App\Model\Entity\User;
 
   class Home extends Page{
 
     // método responsável por retornar o conteúdo (view) da Home
     public static function getHome(){
 
-      $objUser = new User;
-
       // view da home
       $content = View::render('pages/home', [
-        'username'    => $objUser->username,
-        'nickname'    => $objUser->nickname,
-        'email'       => $objUser->email,
-        'password'    => $objUser->password,
-        'birthDate'   => $objUser->birthDate,
-        'image'       => $objUser->image
+        'header'  => self::getHeader(),
+        'footer'  => self::getFooter()
       ]);
 
       // retornar a view da página
-      return parent::getPage('Projeto Interdisciplinar | Desbug', $content);
-    }    
+      return parent::getPage('Home | Desbug', $content);
+    }
+
+    // método responsável por renderizar o topo da página
+    private static function getHeader(){
+      return View::render('pages/header');
+    }
+
+    // método responsável por renderizar o rodapé da página
+    private static function getFooter(){
+      return View::render('pages/footer');
+    }
   }
 ?>
