3ba92c0523f5517da05bbced46d9b3384c898c0d
Marcos Albuquerque <marcos.albuquerque346@gmail.com>
2021-10-26 22:43:43 -0300

    improvement: implementação de método na entidade tb_tempo_ativo

--- app/Model/Entity/ActiveTime.php
+++ app/Model/Entity/ActiveTime.php
@@ -1,11 +1,50 @@
 <?php
-
   namespace App\Model\Entity;
 
+  use \PDO;
+  use App\Db\Database;
+  use App\Model\Entity\User;
+
   Class ActiveTime {
-    public $id = 1; 
-    public $dayAtive = 1;
-    public $inputData = '12/10/2021';
-    public $outputData = '12/10/2021';
-  }
+    public $id;
+    public $User;
+    public $activeDay;
+    public $entryDate;
+    public $departureDate;
+
+    // método responsável por instânciar a classe
+    public function __construct(){
+      $this->User = new User();
+    }
+  
+    // método responsável por inserir depoimentos no banco
+    public function insert(){
+      // definir valores
+      $this->departureDate = null;
+      $this->activeDay = date('z');
+      $this->data = date('Y-m-d H:i:s');
+
+      // insere tempo de atividade no banco de dados
+      $this->id = (new Database('tb_tempo_ativo'))->insert([
+        'id_usuario'    => $this->User->id,
+        'nr_dia_ativo'  => $this->activeDay,
+        'dt_entrada'    => $this->entryDate,
+        'dt_saida'      => $this->departureDate
+      ]);
+
+      // retornar sucesso
+      return true;
+    }
+
+    // método responsável por atualizar depoimentos no banco  
+    public function update(){
+      $objDatabase = new Database('tb_tempo_ativo');
+      $objDatabase->update('id = '.$this->id, [
+        'id_usuario'    => $this->User->id,
+        'nr_dia_ativo'  => $this->activeDay,
+        'dt_entrada'    => $this->entryDate,
+        'dt_saida'      => $this->departureDate
+      ]);
+    }
+  }  
 ?>
