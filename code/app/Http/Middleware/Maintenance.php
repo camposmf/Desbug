4cd84e50f846b465454ae4a44b7a6a8d7ed76300
Marcos Albuquerque <marcos.albuquerque346@gmail.com>
2021-11-08 12:16:27 -0300

    feature: implementação do middleware responsável por controlar se a aplicação estão em manutenção

---
+++ code/app/Http/Middleware/Maintenance.php
@@ -0,0 +1,15 @@
+<?php 
+    namespace App\Http\Middleware;
+
+    class Maintenance {
+        public function handle($request, $next){
+            // verifica estado de manutenção da página
+            if(getenv('MAINTENANCE') == 'true'){
+                throw new \Exception('Página em manutenção tente novamente mais tarde', 200);
+            }
+            
+            // executa o próximo nível do middleware
+            return $next($request);
+        }
+    }
+?>
