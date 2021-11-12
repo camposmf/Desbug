62a22edb93bffc0c8a4fa255e527d9e9737b1462
Marcos Albuquerque <marcos.albuquerque346@gmail.com>
2021-11-12 08:29:02 -0300

    feature: adicionando api para consultar as informações do projeto

--- code/routes/api/default.php
+++ code/routes/api/default.php
@@ -1,10 +1,14 @@
 <?php 
     use \App\Http\Response;
+    use \App\Controller\Api;
 
     // rota raiz da api
     $objRouter->get('/api', [
+        'middlewares' => [
+            'api'
+        ],
         function($request){
-            return new Response(200, 'Api é top!');
+            return new Response(200, Api\Api::getDetails($request), 'application/json');
         }
-    ])
+    ]);
 ?>
